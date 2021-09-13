963f4c020f1ad23eea09ba7daf70c3795d0389cb
sid6145 <sidtdesh@gmail.com>
2021-09-13 18:36:00 +0530

    understood the concepts of bind,apply and call

---
+++ day10/script.js
@@ -0,0 +1,38 @@
+//Bind keyword 
+
+var pokemon = {
+    firstname: 'Pika',
+    lastname: 'Chu ',
+    getPokeName: function() {
+        var fullname = this.firstname + ' ' + this.lastname;
+        return fullname;
+    }
+};
+
+var pokemonName = function(snack, hobby) {
+    console.log(this.getPokeName() + 'I choose you!');
+    console.log(this.getPokeName() + ' loves ' + snack + ' and ' + hobby);
+};
+ // creates new object and binds pokemon. 'this' of pokemon === pokemon now
+var logPokemon = pokemonName.bind(pokemon);
+
+// Pika Chu  loves sushi and algorithms
+logPokemon('sushi', 'algorithms'); 
+
+
+//Apply and Call keyword
+var pokemon = {
+    firstname: 'Pika',
+    lastname: 'Chu ',
+    getPokeName: function() {
+        var fullname = this.firstname + ' ' + this.lastname;
+        return fullname;
+    }
+};
+
+var pokemonName = function(snack, hobby) {
+    console.log(this.getPokeName() + ' loves ' + snack + ' and ' + hobby);
+};
+
+pokemonName.call(pokemon,'sushi', 'algorithms'); // Pika Chu  loves sushi and algorithms
+pokemonName.apply(pokemon,['sushi', 'algorithms']); // Pika Chu  loves sushi and algorithms
